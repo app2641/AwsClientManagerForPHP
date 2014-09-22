c220d80782dd9c950c410cc3a7bfa65bb946be1a
app2641 <app2641@gmail.com>
2014-09-22 11:53:05 +0900

    bug fix

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,6 +3,5 @@
 defined('ROOT') || define('ROOT', realpath(__DIR__.'/../'));
 defined('LIB') || define('LIB', ROOT.'/src');
 
-$loader = require_once ROOT.'/vendor/autoload.php';
-$loader->set('Acf', LIB);
+require_once ROOT.'/vendor/autoload.php';
 
